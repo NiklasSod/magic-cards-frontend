9536cacfbc7b4dbe2a84204bbe75f856e58e56fe
NiklasSod <nma.prob@gmail.com>
2022-08-22 10:24:47 +0200

    fix eslint remove react starter files

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,12 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-extraneous-dependencies */
 module.exports = {
-    babel: {
-        plugins: ["babel-plugin-macros"],
+  babel: {
+    plugins: ['babel-plugin-macros'],
+  },
+  style: {
+    postcssOptions: {
+      plugins: [require('tailwindcss'), require('autoprefixer')],
     },
-    style: {
-        postcssOptions: {
-            plugins: [require("tailwindcss"), require("autoprefixer")],
-        },
-    },
+  },
 };
