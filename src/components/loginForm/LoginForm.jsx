20e62e5f8efa0b3a946fa5cc4348e6a566830a9c
Georgios <ulidin@gmail.com>
2022-08-26 11:21:07 +0200

    remove scrollbar

--- src/components/loginForm/LoginForm.jsx
+++ src/components/loginForm/LoginForm.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 export default function LoginForm() {
   return (
-    <div className="relative flex flex-col justify-center min-h-screen overflow-hidden m-8">
+    <div className="relative flex flex-col justify-center mt-12 overflow-hidden m-8">
       <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-rose-600/40 ring ring-2 ring-purple-600 lg:max-w-xl">
         <h1 className="text-3xl font-semibold text-center text-purple-700 underline uppercase decoration-wavy">
           Sign in
