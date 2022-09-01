f474453867be055172048a071e6e9ccfc781f6f0
Georgios <ulidin@gmail.com>
2022-09-01 11:30:05 +0200

    converted to style components

--- src/components/loginForm/LoginForm.jsx
+++ src/components/loginForm/LoginForm.jsx
@@ -1,22 +1,32 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
+
+import {
+  LoginContainer,
+  LoginInnerContainer,
+  LoginLabel,
+  Input,
+} from "./login.style";
+
 export default function LoginForm() {
   return (
-    <div className="relative flex flex-col justify-center mt-12 overflow-hidden m-8">
-      <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-rose-600/40 ring ring-2 ring-purple-600 lg:max-w-xl">
+    // <div className="relative flex flex-col justify-center mt-12 overflow-hidden m-8">
+    <LoginContainer>
+      {/* <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-rose-600/40 ring ring-2 ring-purple-600 lg:max-w-xl"> */}
+      <LoginInnerContainer>
         <h1 className="text-3xl font-semibold text-center text-purple-700 underline uppercase decoration-wavy">
           Sign in
         </h1>
         <form className="mt-6">
           <div className="mb-2">
-            <label
+            <LoginLabel
               htmlFor="email"
-              className="block text-sm font-semibold text-gray-800"
+              // className="block text-sm font-semibold text-gray-800"
             >
               Email
-            </label>
-            <input
+            </LoginLabel>
+            <Input
               type="email"
-              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              // className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
             />
           </div>
           <div className="mb-2">
@@ -26,9 +36,9 @@ export default function LoginForm() {
             >
               Password
             </label>
-            <input
+            <Input
               type="password"
-              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              // className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
             />
           </div>
           <a href="Test" className="text-xs text-purple-600 hover:underline">
@@ -54,7 +64,7 @@ export default function LoginForm() {
             Sign up
           </a>
         </p>
-      </div>
-    </div>
+      </LoginInnerContainer>
+    </LoginContainer>
   );
 }
---
+++ src/components/loginForm/login.style.js
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+// import tw from "tailwind-styled-components";
+
+export const LoginContainer = styled.div`
+  flex: none;
+  position: relative;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 12px;
+  overflow: hidden;
+  margin: 8px;
+`;
+
+export const LoginInnerContainer = styled.div`
+  width: full;
+  padding: 6px;
+  margin: auto;
+  background: white;
+  border-radius: 0.375rem;
+  box-shadow: 0 20px 25px -5px rgb(0 0 0 / 0.1),
+    0 8px 10px -6px rgb(0 0 0 / 0.1);
+  --tw-ring-color: rgb(225 29 72);
+  box-shadow: var(--tw-ring-inset) 0 0 0 calc(3px + var(--tw-ring-offset-width))
+    var(--tw-ring-color);
+  box-shadow: var(--tw-ring-inset) 0 0 0 calc(2px + var(--tw-ring-offset-width))
+    var(--tw-ring-color);
+  --tw-ring-color: rgb(147 51 234);
+  /* lg:max-w-xl */
+  max-width: 36rem;
+`;
+
+export const LoginLabel = styled.div`
+  display: block;
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  font-weight: 600;
+  color: rgb(31 41 55);
+`;
+
+export const Input = styled.input`
+  display: block;
+  width: 100%;
+  padding-left: 4px;
+  padding-right: 4px;
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+  margin-top: 2px;
+  color: rgb(126 34 206);
+  background: white;
+  border-width: 1px;
+  border-radius: 0.375rem;
+  &:focus {
+    border-color: rgb(192 132 252);
+    --tw-ring-color: rgb(216 180 254);
+    outline: none;
+    box-shadow: var(--tw-ring-inset) 0 0 0
+      calc(3px + var(--tw-ring-offset-width)) var(--tw-ring-color);
+    --tw-ring-opacity: 0.4;
+  }
+`;
