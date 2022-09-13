7ffe4871c71923f894c07d1b78b834815e2d32a8
NiklasSod <nma.prob@gmail.com>
2022-09-13 19:50:00 +0200

    form for forgot password

--- src/components/loginForm/LoginForm.jsx
+++ src/components/loginForm/LoginForm.jsx
@@ -1,5 +1,8 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import ForgotPassBtn from "../forgotPassword/ForgotPassBtn";
+import ForgotPassForm from "../forgotPassword/ForgotPassForm";
 
 import {
   LoginContainer,
@@ -9,64 +12,60 @@ import {
 } from "./login.style";
 
 export default function LoginForm() {
-  return (
-    // <div className="relative flex flex-col justify-center mt-12 overflow-hidden m-8">
-    <LoginContainer>
-      {/* <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-rose-600/40 ring ring-2 ring-purple-600 lg:max-w-xl"> */}
-      <LoginInnerContainer>
-        <h1 className="text-3xl font-semibold text-center text-purple-700 underline uppercase decoration-wavy">
-          Sign in
-        </h1>
-        <form className="mt-6">
-          <div className="mb-2">
-            <LoginLabel
-              htmlFor="email"
-              // className="block text-sm font-semibold text-gray-800"
-            >
-              Email
-            </LoginLabel>
-            <Input
-              type="email"
-              // className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            />
-          </div>
-          <div className="mb-2">
-            <label
-              htmlFor="password"
-              className="block text-sm font-semibold text-gray-800"
-            >
-              Password
-            </label>
-            <Input
-              type="password"
-              // className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            />
-          </div>
-          <a href="Test" className="text-xs text-purple-600 hover:underline">
-            Forget Password?
-          </a>
-          <div className="mt-6">
-            <button
-              type="button"
-              className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600"
-            >
-              Login
-            </button>
-          </div>
-        </form>
+  const [newPassForm, setNewPassForm] = useState(false);
 
-        <p className="mt-8 text-xs font-light text-center text-gray-700">
-          {" "}
-          Don´t have an account?{" "}
-          <Link
-            to="/register"
-            href="#Test"
-            className="font-medium text-purple-600 hover:underline"
-          >
-            Sign up
-          </Link>
-        </p>
-      </LoginInnerContainer>
-    </LoginContainer>
+  const passwordResetForm = () => {
+    setNewPassForm(true);
+  };
+
+  useEffect(() => {}, [newPassForm]);
+
+  return (
+    <>
+      <LoginContainer>
+        <LoginInnerContainer>
+          <h1 className="text-3xl font-semibold text-center text-purple-700 underline uppercase decoration-wavy">
+            Sign in
+          </h1>
+          <form className="mt-6">
+            <div className="mb-2">
+              <LoginLabel htmlFor="email">Email</LoginLabel>
+              <Input type="email" />
+            </div>
+            <div className="mb-2">
+              <label
+                htmlFor="password"
+                className="block text-sm font-semibold text-gray-800"
+              >
+                Password
+              </label>
+              <Input type="password" />
+            </div>
+            <div className="mt-6">
+              <button
+                type="button"
+                className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600"
+              >
+                Login
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-8 text-xs font-light text-center text-gray-700">
+            {" "}
+            Don´t have an account?{" "}
+            <Link
+              to="/register"
+              href="#Test"
+              className="font-medium text-purple-600 hover:underline"
+            >
+              Sign up
+            </Link>
+          </p>
+        </LoginInnerContainer>
+      </LoginContainer>
+      {!newPassForm && <ForgotPassBtn passwordResetForm={passwordResetForm} />}
+      {newPassForm && <ForgotPassForm />}
+    </>
   );
 }
